4d1958d31eb03e3ba28715fb93400f1f3184f224
thiagoabento <thiagoabento@hotmail.com>
2025-09-05 19:58:06 -0300

    subindo pasta de teste

---
+++ tests/test_transform.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from etl_vendas_1.etl.transform import incluir_coluna_total
+
+
+def test_transform_data_return_colum_total():
+    df = pd.DataFrame({
+        'quantidade': [1, 2, 3],
+        'preco': [10, 15, 30]
+    })
+
+    df_result = incluir_coluna_total(df)
+
+    assert 'total' in df_result.columns
+    assert df_result['total'].tolist() == [10, 30, 90]
+
+
+
