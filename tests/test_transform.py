ceeaf388f2959f0eceef4a82b21ddb2a496616a8
thiagoabento <thiagoabento@hotmail.com>
2025-09-05 20:24:05 -0300

    atualizando arquivo de teste

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -4,14 +4,14 @@ from etl.transform import incluir_coluna_total
 
 def test_transform_data_return_colum_total():
     df = pd.DataFrame({
-        'quantidade': [1, 2, 3],
+        'quantidade': [1, 2, 1],
         'preco': [10, 15, 30]
     })
 
     df_result = incluir_coluna_total(df)
 
     assert 'total' in df_result.columns
-    assert df_result['total'].tolist() == [10, 30, 90]
+    assert df_result['total'].tolist() == [10, 30, 30]
 
 
 
